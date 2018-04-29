5712715f0dad21225c9f6e27240b232df9206791
volcy Samson <volcysamson@gmail.com>
2018-04-29 19:47:17 -0400

    Added user Profile migrate,model and updated user seeder

--- app/User.php
+++ app/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function run()
+    {
+        $this->hasOne('App/User');
+    }
 }
